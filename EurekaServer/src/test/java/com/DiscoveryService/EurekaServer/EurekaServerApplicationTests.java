b150115bd02ba2823425e923d4e95a67d84866bc
Ravi Teja <67220989+raviteja950@users.noreply.github.com>
2026-04-28 13:10:34 +0530

    Add files via upload

---
+++ EurekaServer/src/test/java/com/DiscoveryService/EurekaServer/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DiscoveryService.EurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
